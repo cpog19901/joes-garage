86373f5315db0676a0d07fa24159a280959bc981
Ciaran O'Grady <ciaranogrady8@gmail.com>
2021-07-09 16:58:07 +0100

    remove unused imports

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -1,7 +1,5 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React from "react";
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 
--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CarItem = (props) => {
 
-const [linkValue, setLinkValue] = useState("");   
+// const [linkValue, setLinkValue] = useState("");   
 
 
 
