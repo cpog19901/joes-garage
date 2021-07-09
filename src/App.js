63e352b5f46f477ee8afead1e26c6e95d11da91b
Ciaran O'Grady <ciaranogrady8@gmail.com>
2021-07-09 11:43:02 +0100

    make api request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+
 
 function App() {
 
-   useEffect(()=>{
-  axios.get("https://react-challenge-api.azurewebsites.net/vehicles")
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error =>{
-    console.log(error);
-  })
+  const [cars, setCars] = useState([]);
+//fetch API data
 
+  useEffect(()=>{
+    axios.get("https://react-challenge-api.azurewebsites.net/vehicles")
+    .then(response => {
+      console.log(response.data);
+      setCars(response.data);
+      console.log(cars)
+      
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }, [])
+  
+  
   return (
     <div className="App">
-    
+      {}
     </div>
   );
 }
-
 export default App;
